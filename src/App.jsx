503a37965c05a20a9d523826a0a3a0ac98d2d229
Broodco <broodcoorens.maxime@gmail.com>
2023-07-14 10:58:47 +0200

    Implemented list of todos status change'

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import AddTodo from "./components/AddTodo.jsx";
 import ListTodos from "./components/ListTodos.jsx";
 
 export default function App() {
+/*  const handleClick = () => {
+    const newTodos = [...todos];
+    newTodos.push("This is a new todo");
+    setTodos(newTodos);
+  }*/
+
   return (
     <main className="container">
       <Header />
