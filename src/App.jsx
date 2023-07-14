77605fd0326f90ee236747aa6de991715d847581
Broodco <broodcoorens.maxime@gmail.com>
2023-07-14 13:57:34 +0200

    Implement localstorage storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import Header from "./components/Header.jsx";
 import AddTodo from "./components/AddTodo.jsx";
 import ListTodos from "./components/ListTodos.jsx";
-import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect, useState } from "react";
+
+const LSKEY = "MyTodoApp";
 
 export default function App() {
-  const initTodos = [
-    {
-      id: '0',
-      name: 'Apprendre React',
-      done: false
-    },
-    {
-      id: '1',
-      name: 'Faire la vaisselle',
-      done: true
-    },
-    {
-      id: '2',
-      name: 'Faire un jogging',
-      done: false
-    }
-  ];
-  const [todos, setTodos] = useState(initTodos);
+  const [todos, setTodos] = useState([]);
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const changeTodoStatus = (id) => {
     const newTodos = todos.map((todo) => {
@@ -35,7 +22,7 @@ export default function App() {
 
   const addNewTodo = (newTodoName) => {
     const newTodo = {
-      id: todos.length.toString(),
+      id: uuidv4(),
       name: newTodoName,
       done: false
     };
@@ -43,6 +30,19 @@ export default function App() {
     newTodos.push(newTodo);
     setTodos(newTodos);
   }
+
+  useEffect(() => {
+    const initialTodos = JSON.parse(window.localStorage.getItem(LSKEY + '.todos'))
+    setTodos(initialTodos);
+  }, []);
+
+  useEffect(() => {
+    if (!firstLoad) {
+      window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+    } else {
+      setFirstLoad(false);
+    }
+  }, [todos, firstLoad]);
 
   return (
     <main className="container">
